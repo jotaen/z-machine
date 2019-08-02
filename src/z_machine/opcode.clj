1a4393fa48a6c3a9078ed01ab3d11821ae222278
Jan Heuermann <jan@jotaen.net>
2019-08-02 17:33:25 +0200

    Prefix symbols

--- src/z_machine/opcode.clj
+++ src/z_machine/opcode.clj
@@ -5,52 +5,52 @@
 
 (defn get-form [byte]
   (cond
-    (= byte 190) :extended
-    (= (bits-extract byte 0 2) 2r11) :variable
-    (= (bits-extract byte 0 2) 2r10) :short
-    :else :long))
+    (= byte 190) :form-extended
+    (= (bits-extract byte 0 2) 2r11) :form-variable
+    (= (bits-extract byte 0 2) 2r10) :form-short
+    :else :form-long))
 
 (defn get-operand-count [byte]
   (case (get-form byte)
-    :variable (if (= (bits-extract byte 4 1) 2r1) :VAR :2OP)
-    :short (if (= (bits-extract byte 3 2) 2r11) :0OP :1OP)
-    :extended :VAR
-    :long :2OP))
+    :form-short (if (= (bits-extract byte 3 2) 2r11) :0OP :1OP)
+    :form-long :2OP
+    :form-variable (if (= (bits-extract byte 4 1) 2r1) :VAROPS :2OP)
+    :form-extended :VAROPS))
 
 (defn get-opcode-number [bytes]
   (let [[b1 b2] bytes]
     (case (get-form b1)
-      :short (bits-extract b1 4 4)
-      :long (bits-extract b1 3 5)
-      :variable (bits-extract b1 3 5)
-      :extended b2)))
+      :form-short (bits-extract b1 4 4)
+      :form-long (bits-extract b1 3 5)
+      :form-variable (bits-extract b1 3 5)
+      :form-extended b2)))
 
 (defn get-operand-types [bytes]
   (defn two-digit-type [byte]
     (case byte
-      2r00 :large-constant
-      2r01 :small-constant
-      2r10 :variable
-      2r11 :omitted))
+      2r00 :type-large-constant
+      2r01 :type-small-constant
+      2r10 :type-a-variable
+      2r11 :type-omitted))
   (defn one-digit-type [byte]
     (case byte
-      2r0 :small-constant
-      2r1 :variable))
+      2r0 :type-small-constant
+      2r1 :type-a-variable))
   (let [[b1 b2 b3] bytes]
     (case (get-form b1)
-      :short [
+      :form-short [
         (two-digit-type (bits-extract b1 3 2))]
-      :long [
+      :form-long [
         (one-digit-type (bits-extract b1 5 1))
         (one-digit-type (bits-extract b1 4 1))]
-      :variable [
+      :form-variable [
         (two-digit-type (bits-extract b2 6 2))
         (two-digit-type (bits-extract b2 4 2))
         (two-digit-type (bits-extract b2 2 2))
         (two-digit-type (bits-extract b2 0 2))
       ]
-      :extended [
-        ; in extended form b2 is used for the opcode number
+      :form-extended [
+        ; in form-extended form b2 is used for the opcode number
         (two-digit-type (bits-extract b3 6 2))
         (two-digit-type (bits-extract b3 4 2))
         (two-digit-type (bits-extract b3 2 2))
--- test/z_machine/core_test.clj
+++ test/z_machine/core_test.clj
@@ -11,67 +11,67 @@
     (is (= (bits-extract 2r10101010 4 2) 2r00000010))))
 
 (deftest test-get-form
-  (testing "`short`"
-    (is (= (get-form 2r10000000) :short))
-    (is (= (get-form 2r10000111) :short))
-    (is (= (get-form 2r10100000) :short)))
-  (testing "`long`"
-    (is (= (get-form 2r01011110) :long))
-    (is (= (get-form 2r00010111) :long))
-    (is (= (get-form 2r00001000) :long)))
-  (testing "`variable`"
-    (is (= (get-form 2r11000000) :variable))
-    (is (= (get-form 2r11001100) :variable))
-    (is (= (get-form 2r11111111) :variable)))
-  (testing "`extended`"
-    (is (= (get-form 190) :extended))))
+  (testing "`form-short`"
+    (is (= (get-form 2r10000000) :form-short))
+    (is (= (get-form 2r10000111) :form-short))
+    (is (= (get-form 2r10100000) :form-short)))
+  (testing "`form-long`"
+    (is (= (get-form 2r01011110) :form-long))
+    (is (= (get-form 2r00010111) :form-long))
+    (is (= (get-form 2r00001000) :form-long)))
+  (testing "`form-variable`"
+    (is (= (get-form 2r11000000) :form-variable))
+    (is (= (get-form 2r11001100) :form-variable))
+    (is (= (get-form 2r11111111) :form-variable)))
+  (testing "`form-extended`"
+    (is (= (get-form 190) :form-extended))))
 
 (deftest test-get-operand-count
-  (testing "`short`"
+  (testing "`form-short`"
     (is (= (get-operand-count 2r10011000) :0OP))
     (is (= (get-operand-count 2r10000000) :1OP))
     (is (= (get-operand-count 2r10001000) :1OP))
     (is (= (get-operand-count 2r10010000) :1OP)))
-  (testing "`long`"
+  (testing "`form-long`"
     (is (= (get-operand-count 2r01011110) :2OP)))
-  (testing "`variable`"
-    (is (= (get-operand-count 2r11001000) :VAR))
+  (testing "`form-variable`"
+    (is (= (get-operand-count 2r11001000) :VAROPS))
     (is (= (get-operand-count 2r11000000) :2OP)))
-  (testing "`extended`"
-    (is (= (get-operand-count 190) :VAR))))
+  (testing "`form-extended`"
+    (is (= (get-operand-count 190) :VAROPS))))
 
 (deftest test-get-opcode-number
-  (testing "`short`"
+  (testing "`form-short`"
     (is (= (get-opcode-number [2r10010101]) 5))
     (is (= (get-opcode-number [2r10011000]) 8))
     (is (= (get-opcode-number [2r10011001]) 9)))
-  (testing "`long`"
+  (testing "`form-long`"
     (is (= (get-opcode-number [2r01000000]) 0))
     (is (= (get-opcode-number [2r01011010]) 26))
     (is (= (get-opcode-number [2r01011110]) 30)))
-  (testing "`variable`"
+  (testing "`form-variable`"
     (is (= (get-opcode-number [2r11001000]) 8))
     (is (= (get-opcode-number [2r11001011]) 11)))
-  (testing "`extended`"
+  (testing "`form-extended`"
     (is (= (get-opcode-number [190 7]) 7))))
 
 (deftest test-get-operand-types
-  (testing "`short`"
-    (is (= (get-operand-types [2r10000000]) [:large-constant]))
-    (is (= (get-operand-types [2r10001000]) [:small-constant]))
-    (is (= (get-operand-types [2r10010000]) [:variable]))
-    (is (= (get-operand-types [2r10011000]) [:omitted])))
-  (testing "`long`"
-    (is (= (get-operand-types [2r01000000]) [:small-constant :small-constant]))
-    (is (= (get-operand-types [2r01000100]) [:variable :small-constant]))
-    (is (= (get-operand-types [2r01001000]) [:small-constant :variable]))
-    (is (= (get-operand-types [2r01001100]) [:variable :variable])))
-  (testing "`variable`"
-    (is (= (get-operand-types [2r11001000 2r11011000]) [:large-constant :variable :small-constant :omitted]))
-    (is (= (get-operand-types [2r11001000 2r11111101]) [:small-constant :omitted :omitted :omitted])))
-  (testing "`extended`"
-    (is (= (get-operand-types [190 2r11111100 2r11011000]) [:large-constant :variable :small-constant :omitted]))))
-  ;; TODO assert that ommited must be be followed by omitted
+  (testing "`form-short`"
+    (is (= (get-operand-types [2r10000000]) [:type-large-constant]))
+    (is (= (get-operand-types [2r10001000]) [:type-small-constant]))
+    (is (= (get-operand-types [2r10010000]) [:type-a-variable]))
+    (is (= (get-operand-types [2r10011000]) [:type-omitted])))
+  (testing "`form-long`"
+    (is (= (get-operand-types [2r01000000]) [:type-small-constant :type-small-constant]))
+    (is (= (get-operand-types [2r01000100]) [:type-a-variable :type-small-constant]))
+    (is (= (get-operand-types [2r01001000]) [:type-small-constant :type-a-variable]))
+    (is (= (get-operand-types [2r01001100]) [:type-a-variable :type-a-variable])))
+  (testing "`form-variable`"
+    (is (= (get-operand-types [2r11001000 2r11011000]) [:type-large-constant :type-a-variable :type-small-constant :type-omitted]))
+    (is (= (get-operand-types [2r11001000 2r11111101]) [:type-small-constant :type-omitted :type-omitted :type-omitted])))
+  (testing "`form-extended`"
+    (is (= (get-operand-types [190 2r11111100 2r11011000]) [:type-large-constant :type-a-variable :type-small-constant :type-omitted]))))
+  ;; TODO assert that ommited must be be followed by type-omitted
   ;; TODO handle special case mentioned in 4.4.3.1 (double variable VAR opcodes)
 
 
