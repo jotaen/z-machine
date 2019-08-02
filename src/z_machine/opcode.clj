33c02dff4eae0a369c27082d594833b1ccf0a4dc
Jan Heuermann <jan@jotaen.net>
2019-08-02 12:41:07 +0200

    Analyse operand count

--- src/z_machine/opcode.clj
+++ src/z_machine/opcode.clj
@@ -3,9 +3,22 @@
 (defn extract-first-two-bits [b]
   (bit-and b 2r11000000))
 
+(defn extract-fifth-bit [b]
+  (bit-and b 2r00001000))
+
+(defn extract-fourth-and-fifth-bit [b]
+  (bit-and b 2r00011000))
+
 (defn get-form [byte]
   (cond
     (= byte 190) :extended
     (= (extract-first-two-bits byte) 2r11000000) :variable
     (= (extract-first-two-bits byte) 2r10000000) :short
     :else :long))
+
+(defn get-operand-count [byte]
+  (case (get-form byte)
+    :variable (if (= (extract-fifth-bit byte) 2r00001000) :VAR :2OP)
+    :short (if (= (extract-fourth-and-fifth-bit byte) 2r00011000) :0OP :1OP)
+    :extended :VAR
+    :long :2OP))
--- test/z_machine/core_test.clj
+++ test/z_machine/core_test.clj
@@ -2,19 +2,32 @@
   (:require [clojure.test :refer :all]
             [z-machine.opcode :refer :all]))
 
-(deftest a-test
-  (testing "`extended`"
-    (is (= (get-form 190) :extended)))
+(deftest test-get-form
   (testing "`short`"
     (is (= (get-form 2r10000000) :short))
     (is (= (get-form 2r10000111) :short))
     (is (= (get-form 2r10100000) :short)))
-  (testing "`variable`"
-    (is (= (get-form 2r11000000) :variable))
-    (is (= (get-form 2r11001100) :variable))
-    (is (= (get-form 2r11111111) :variable)))
   (testing "`long`"
     (is (= (get-form 2r01011110) :long))
     (is (= (get-form 2r01010111) :long))
     (is (= (get-form 2r01000000) :long)))
-  )
+  (testing "`variable`"
+    (is (= (get-form 2r11000000) :variable))
+    (is (= (get-form 2r11001100) :variable))
+    (is (= (get-form 2r11111111) :variable)))
+  (testing "`extended`"
+    (is (= (get-form 190) :extended))))
+
+(deftest test-get-operand-count
+  (testing "`short`"
+    (is (= (get-operand-count 2r10011000) :0OP))
+    (is (= (get-operand-count 2r10000000) :1OP))
+    (is (= (get-operand-count 2r10001000) :1OP))
+    (is (= (get-operand-count 2r10010000) :1OP)))
+  (testing "`long`"
+    (is (= (get-operand-count 2r01011110) :2OP)))
+  (testing "`variable`"
+    (is (= (get-operand-count 2r11001000) :VAR))
+    (is (= (get-operand-count 2r11000000) :2OP)))
+  (testing "`extended`"
+    (is (= (get-operand-count 190) :VAR))))
