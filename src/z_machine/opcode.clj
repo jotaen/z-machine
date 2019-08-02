4621428fe27f3f8052f680729f8b6383e41de24f
Jan Heuermann <jan@jotaen.net>
2019-08-02 16:53:26 +0200

    add operand types parsing

--- src/z_machine/opcode.clj
+++ src/z_machine/opcode.clj
@@ -1,37 +1,58 @@
 (ns z-machine.opcode)
 
-(defn extract-first-two-bits [b]
-  (bit-and b 2r11000000))
-
-(defn extract-fifth-bit [b]
-  (bit-and b 2r00001000))
-
-(defn extract-fourth-and-fifth-bit [b]
-  (bit-and b 2r00011000))
-
-(defn extract-last-four-bits [b]
-  (bit-and b 2r00001111))
-
-(defn extract-last-five-bits [b]
-  (bit-and b 2r00011111))
+(defn bits-extract [byte start len]
+  (bit-shift-right (bit-and (bit-shift-left byte start) 2r11111111) (- 8 len)))
 
 (defn get-form [byte]
   (cond
     (= byte 190) :extended
-    (= (extract-first-two-bits byte) 2r11000000) :variable
-    (= (extract-first-two-bits byte) 2r10000000) :short
+    (= (bits-extract byte 0 2) 2r11) :variable
+    (= (bits-extract byte 0 2) 2r10) :short
     :else :long))
 
 (defn get-operand-count [byte]
   (case (get-form byte)
-    :variable (if (= (extract-fifth-bit byte) 2r00001000) :VAR :2OP)
-    :short (if (= (extract-fourth-and-fifth-bit byte) 2r00011000) :0OP :1OP)
+    :variable (if (= (bits-extract byte 4 1) 2r1) :VAR :2OP)
+    :short (if (= (bits-extract byte 3 2) 2r11) :0OP :1OP)
     :extended :VAR
     :long :2OP))
 
 (defn get-opcode-number [bytes]
-  (case (get-form (first bytes))
-    :short (extract-last-four-bits (first bytes))
-    :long (extract-last-five-bits (first bytes))
-    :variable (extract-last-five-bits (first bytes))
-    :extended (second bytes)))
+  (let [[b1 b2] bytes]
+    (case (get-form b1)
+      :short (bits-extract b1 4 4)
+      :long (bits-extract b1 3 5)
+      :variable (bits-extract b1 3 5)
+      :extended b2)))
+
+(defn get-operand-types [bytes]
+  (defn two-digit-type [byte]
+    (case byte
+      2r00 :large-constant
+      2r01 :small-constant
+      2r10 :variable
+      2r11 :omitted))
+  (defn one-digit-type [byte]
+    (case byte
+      2r0 :small-constant
+      2r1 :variable))
+  (let [[b1 b2 b3] bytes]
+    (case (get-form b1)
+      :short [
+        (two-digit-type (bits-extract b1 3 2))]
+      :long [
+        (one-digit-type (bits-extract b1 5 1))
+        (one-digit-type (bits-extract b1 4 1))]
+      :variable [
+        (two-digit-type (bits-extract b2 6 2))
+        (two-digit-type (bits-extract b2 4 2))
+        (two-digit-type (bits-extract b2 2 2))
+        (two-digit-type (bits-extract b2 0 2))
+      ]
+      :extended [
+        ; in extended form b2 is used for the opcode number
+        (two-digit-type (bits-extract b3 6 2))
+        (two-digit-type (bits-extract b3 4 2))
+        (two-digit-type (bits-extract b3 2 2))
+        (two-digit-type (bits-extract b3 0 2)) 
+      ])))
--- test/z_machine/core_test.clj
+++ test/z_machine/core_test.clj
@@ -2,6 +2,14 @@
   (:require [clojure.test :refer :all]
             [z-machine.opcode :refer :all]))
 
+(deftest test-bits-extract
+  (testing "it all"
+    (is (= (bits-extract 2r01000000 0 2) 2r00000001))
+    (is (= (bits-extract 2r01001010 2 5) 2r00000101))
+    (is (= (bits-extract 2r01001010 0 8) 2r01001010))
+    (is (= (bits-extract 2r01001010 0 0) 2r00000000))
+    (is (= (bits-extract 2r10101010 4 2) 2r00000010))))
+
 (deftest test-get-form
   (testing "`short`"
     (is (= (get-form 2r10000000) :short))
@@ -9,8 +17,8 @@
     (is (= (get-form 2r10100000) :short)))
   (testing "`long`"
     (is (= (get-form 2r01011110) :long))
-    (is (= (get-form 2r01010111) :long))
-    (is (= (get-form 2r01000000) :long)))
+    (is (= (get-form 2r00010111) :long))
+    (is (= (get-form 2r00001000) :long)))
   (testing "`variable`"
     (is (= (get-form 2r11000000) :variable))
     (is (= (get-form 2r11001100) :variable))
@@ -46,3 +54,21 @@
     (is (= (get-opcode-number [2r11001011]) 11)))
   (testing "`extended`"
     (is (= (get-opcode-number [190 7]) 7))))
+
+(deftest test-get-operand-types
+  (testing "`short`"
+    (is (= (get-operand-types [2r10000000]) [:large-constant]))
+    (is (= (get-operand-types [2r10001000]) [:small-constant]))
+    (is (= (get-operand-types [2r10010000]) [:variable]))
+    (is (= (get-operand-types [2r10011000]) [:omitted])))
+  (testing "`long`"
+    (is (= (get-operand-types [2r01000000]) [:small-constant :small-constant]))
+    (is (= (get-operand-types [2r01000100]) [:variable :small-constant]))
+    (is (= (get-operand-types [2r01001000]) [:small-constant :variable]))
+    (is (= (get-operand-types [2r01001100]) [:variable :variable])))
+  (testing "`variable`"
+    (is (= (get-operand-types [2r11001000 2r11011000]) [:large-constant :variable :small-constant :omitted]))
+    (is (= (get-operand-types [2r11001000 2r11111101]) [:small-constant :omitted :omitted :omitted])))
+  (testing "`extended`"
+    (is (= (get-operand-types [190 2r11111100 2r11011000]) [:large-constant :variable :small-constant :omitted]))))
+  ;; TODO assert that ommited must be be followed by omitted
