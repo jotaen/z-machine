d24cf39d5454c6b06dc5de79c103a4f7ec4bb76f
Jan Heuermann <jan@jotaen.net>
2019-08-23 11:47:38 +0200

    Implement long form instruction parsing

--- src/z_machine/instruction.clj
+++ src/z_machine/instruction.clj
@@ -2,30 +2,33 @@
 
 (defn instruction-names [byte]
   (case byte
-    (0x04 0x24) :dec_chk
-    (0x05 0x25) :inc_chk
+    (0x04 0x24 0x44 0x64) :dec_chk
+    (0x05 0x25 0x45 0x65) :inc_chk
   ))
+
+(defn operands-long-form [bytes]
+  (let [[first second third] bytes]
+    (cond
+      (< first 0x20) [
+        [:type-small-constant second]
+        [:type-small-constant third]]
+      (< first 0x40) [
+        [:type-small-constant second]
+        [:type-variable third]]
+      (< first 0x60)  [
+        [:type-variable second]
+        [:type-small-constant third]]
+      :else [
+          [:type-variable second]
+          [:type-variable third]])))
 
 (defn decode [bytes] 
   (let [[first second third fourth] bytes]
     (cond
-      (< first 0x20) {
+      (<= first 0x7f) {
         :name (instruction-names first)
         :form :form-long
         :opcode first
         :operand-count :2OP
-        :operands [
-          [:type-small-constant second]
-          [:type-small-constant third]
-        ]
-        :branch-offset [fourth]}
-      :else {
-        :name (instruction-names first)
-        :form :form-long
-        :opcode first
-        :operand-count :2OP
-        :operands [
-          [:type-small-constant second]
-          [:type-variable third]
-        ]
+        :operands (operands-long-form bytes)
         :branch-offset [fourth]})))
--- test/z_machine/instruction_test.clj
+++ test/z_machine/instruction_test.clj
@@ -8,6 +8,10 @@
   :dec_chk-SS [0x04 0x12 0xab 0xd5]
   :inc_chk-SV [0x25 0x02 0x00 0xd4]
   :dec_chk-SV [0x24 0x21 0x1e 0xd6]
+  :inc_chk-VS [0x45 0x00 0x02 0xd6]
+  :dec_chk-VS [0x44 0x1e 0x21 0xd4]
+  :inc_chk-VV [0x65 0x10 0x32 0xd5]
+  :dec_chk-VV [0x64 0x32 0x10 0xd7]
 })
 
 (deftest instruction-decoder
@@ -35,7 +39,7 @@
         :branch-offset [0xd5]
       })))
 
-  (testing "range 0x20 0x3f"
+  (testing "range 0x20 to 0x3f"
     (is (= (decode (:inc_chk-SV instructions)) {
         :name :inc_chk
         :form :form-long
@@ -57,5 +61,53 @@
           [:type-variable 0x1e]
         ]
         :branch-offset [0xd6]
+      })))
+
+  (testing "range 0x40 to 0x5f"
+    (is (= (decode (:inc_chk-VS instructions)) {
+        :name :inc_chk
+        :form :form-long
+        :opcode 0x45
+        :operand-count :2OP
+        :operands [
+          [:type-variable 0x00]
+          [:type-small-constant 0x02]
+        ]
+        :branch-offset [0xd6]
+      }))
+    (is (= (decode (:dec_chk-VS instructions)) {
+        :name :dec_chk
+        :form :form-long
+        :opcode 0x44
+        :operand-count :2OP
+        :operands [
+          [:type-variable 0x1e]
+          [:type-small-constant 0x21]
+        ]
+        :branch-offset [0xd4]
+      })))
+
+  (testing "range 0x60 to 0x7f"
+    (is (= (decode (:inc_chk-VV instructions)) {
+        :name :inc_chk
+        :form :form-long
+        :opcode 0x65
+        :operand-count :2OP
+        :operands [
+          [:type-variable 0x10]
+          [:type-variable 0x32]
+        ]
+        :branch-offset [0xd5]
+      }))
+    (is (= (decode (:dec_chk-VV instructions)) {
+        :name :dec_chk
+        :form :form-long
+        :opcode 0x64
+        :operand-count :2OP
+        :operands [
+          [:type-variable 0x32]
+          [:type-variable 0x10]
+        ]
+        :branch-offset [0xd7]
       })))
 )
